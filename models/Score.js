34770181fdb054eac471f476afdbe5177f9285e3
jonp4 <jonp0807@gmail.com>
2025-05-15 04:41:21 -0400

    Add files via upload

---
+++ models/Score.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ScoreSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  },
+  totalQuestions: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  timePerQuestion: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Score', ScoreSchema); 
